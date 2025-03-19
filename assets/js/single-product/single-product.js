5f530c26953ba936fe668b7abe5aa148baabf37c
kali <kali@kali.kali>
2025-03-19 15:30:40 +0300

    catalog and some fixes

---
+++ assets/js/single-product/single-product.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function initSplide(mainSelector, thumbsSelector) {
+    var main = new Splide(mainSelector, {
+      type: "fade",
+      rewind: true,
+      pagination: false,
+      arrows: mainSelector.includes("__modal") ? true : false,
+      breakpoints: {
+        600: {
+          arrows: false,
+        },
+      },
+    });
+
+    var thumbnails = new Splide(thumbsSelector, {
+      fixedWidth: 80,
+      fixedHeight: 80,
+      gap: 8,
+      rewind: true,
+      pagination: false,
+      isNavigation: true,
+      direction: "ttb",
+      heightRatio: 50,
+      arrows: false, // Отключаем стандартные стрелки
+      breakpoints: {
+        600: {
+          fixedWidth: 50,
+          fixedHeight: 50,
+          height: "auto",
+        },
+      },
+    });
+
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
+
+    // Обрабатываем клики по кастомным стрелкам
+    document.querySelector(".custom-splide__arrow--prev").addEventListener("click", function () {
+      thumbnails.go("<");
+    });
+
+    document.querySelector(".custom-splide__arrow--next").addEventListener("click", function () {
+      thumbnails.go(">");
+    });
+  }
+
+  initSplide("#main-carousel", "#thumbnail-carousel");
+});
+
+
+
